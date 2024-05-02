f2bcd7cd5c4eaa077059cffef9a3887fae79b463
Amos SOSSOUKPE <fenouamossossoukpe@gmail.com>
2024-05-02 11:22:45 +0200

    Initialisation du projet

---
+++ tailwind.config.mjs
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	theme: {
+		extend: {
+			colors:{
+				gradient1: "#db03e0",
+				gradient2: "#7b25be",
+				gradient3: "#1d459d",
+			  },
+			  fontFamily: {
+				poppins: ["Poppins", "sans-serif"],
+				
+			  },
+		},
+	},
+	plugins: [],
+}
